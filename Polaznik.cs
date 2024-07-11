4563e7f6a698fab00d946cbde400e3263997e4b8
Janko Gakovski <gakovski.janko@outlook.com>
2024-07-11 18:34:49 +0200

    Add project files.

---
+++ Polaznik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Proizvodi
+{
+
+    public class Polaznik
+    {
+        public delegate void PredajIspit(Polaznik polaznik);
+
+        public event PredajIspit IspitZavrsen;
+
+        public string ImePrezime { get; set; }
+
+        public void OdgovoriNaPitanja(DateTime vrijemePocetka)
+        {
+            Console.WriteLine($"Polaznik {ImePrezime}, je započeo pisanje ispita u: {vrijemePocetka} ");
+        }
+
+        public void PredajOfgovoreNaPitanja()
+        {
+            Console.WriteLine($"Polaznik {ImePrezime} je predao odgovore.");
+            IspitZavrsen?.Invoke(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Proizvodi
+{
+    internal class Program
+    {
+        public delegate void PocniPisatiIspit(DateTime pocetakIspita);
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
